4c371af8d17a00522d029829bbd3acfa77bfb0e0
Resul-Necefli <necefliresul29@gmail.com>
2023-12-09 16:51:11 +0400

    second

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Resul-Necefli/puppy
+
+go 1.21.0
---
+++ puppy.go
@@ -0,0 +1,9 @@
+package puppy
+
+func BreakFast() string {
+	return "Hello go"
+}
+
+func sample() string {
+	return "My name is  Resul"
+}
