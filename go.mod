89c6a21622f2d6fddf2ebccf61369a557e8f4f1e
Resul-Necefli <necefliresul29@gmail.com>
2023-12-13 04:58:32 +0400

    end

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Resul-Necefli/puppy
 
 go 1.21.0
+
+require github.com/Resul-Necefli/animalPackage v0.0.0-20231213000337-2c10a2bfd4bb // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,24 @@
 package puppy
 
+import Retro "github.com/Resul-Necefli/animalPackage"
+
 func BreakFast() string {
 	return "Hello go"
 }
 
 func Sample() string {
 	return "My name is  Resul"
+}
+
+func Melek() string {
+	return Retro.Retros("Orxan Necefli")
+
+}
+
+func BreakFast1() string {
+	return Retro.Retros(BreakFast())
+}
+
+func Sample1() string {
+	return Retro.Retros(Sample())
 }
